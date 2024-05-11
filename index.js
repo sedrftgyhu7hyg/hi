19239802078c45e9c1115e9906128d32a019ee8e
sedrftgyhu7hyg <gs1lameiste@hotmail.com>
2024-05-11 10:43:19 +0700

    update task commit

--- index.js
+++ index.js
@@ -1,19 +1,10 @@
-function combinationSum2(candidates, target) {
-  candidates.sort((a, b) => a - b);
-  const result = [];
-  backtrack([], 0, 0);
-  return result;
-  function backtrack(combination, start, sum) {
-    if (sum === target) {
-      result.push([...combination]);
-      return;
-    }
-    if (sum > target) return;
-    for (let i = start; i < candidates.length; i++) {
-      if (i > start && candidates[i] === candidates[i - 1]) continue;
-      combination.push(candidates[i]);
-      backtrack(combination, i + 1, sum + candidates[i]);
-      combination.pop();
-    }
+function findPeakElement(nums) {
+  let left = 0;
+  let right = nums.length - 1;
+  while (left < right) {
+    const mid = Math.floor((left + right) / 2);
+    if (nums[mid] < nums[mid + 1]) left = mid + 1;
+    else right = mid;
   }
+  return left;
 }
